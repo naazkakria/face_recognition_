64f097a202473049185a21043aa3985dcae350b0
naaz <65398335+naazkakria@users.noreply.github.com>
2021-06-20 01:03:20 +0530

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -1,15 +1,15 @@
 import cv2 
 cap=cv2.VideoCapture(0)
 face_cascade =cv2.CascadeClassifier("C:/Users/HP/Downloads/haarcascade_frontalface_alt.xml")
+
 while True:
-    
-    ret, frame= cap.read() ## capture camera frame, and ret store true and false
+    r, frame= cap.read() ## capture camera frame, and ret store true and false
     gray_frame =cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     
-    if ret==False:
+    if r==False:
         continue
     faces=face_cascade.detectMultiScale(gray_frame,1.3,5)
-    ### 
+  
     """ The first argument is the image, the second is the 
     scalefactor (how much the image size will be reduced at each image scale), 
     and the third is the minNeighbors (how many neighbors each rectangle should have)"""
@@ -19,6 +19,12 @@ while True:
         
     cv2.imshow("video frame",frame)
     key_pressed =cv2.waitKey(1) & 0xFF
+    
+    """cv2.waitKey() returns a 32 Bit integer value (might be dependent on the platform). 
+    The key input is in ASCII which is an 8 Bit integer value. So you only care
+    about these 8 bits and want all other bits to be 0. This you can achieve with:0xFF"""
+    
+    ### ord converts characters in unicode
     if key_pressed ==ord('n'):
         break
         
